6e55cef879a585164020d3870b48293a9dafc76a
trentingram <tingram@homeaway.com>
2019-01-14 20:03:38 +0000

    updated function to console.log sytantris data

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ var requestOptions = {
   };
 
 function getAndPrintHTMLChunks () {
+let body = '';
 
 https.get(requestOptions, function(response){
 
     response.setEncoding('utf8')
 
     response.on('error', function (err){
-        console.log(`${err}`)
+        console.log(`This is your error: ${err}`)
     })
 
-    response.on('data', function(data){
-        console.log(`Data length is: ${data.length}`)
+    response.on('data', function (data){
+        console.log(`Here is some data: ${data}`)
+        
+    })
+
+    response.on('end', function (){
+        console.log('This is the end of this chunk')
+    })
+
+    response.on('finish', function (){
+        console.log('That is the end!')
     })
 
   })
-
+return body
   }
   getAndPrintHTMLChunks()
