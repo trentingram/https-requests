bf1c5fbb644e0d4a8f6058d3580d0254e18a9356
trentingram <tingram@homeaway.com>
2019-01-14 21:26:12 +0000

    adds multiple host/path options object

---
+++ options.js
@@ -0,0 +1,31 @@
+const https = require('https')
+
+const requestOptions = {
+    option1: {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step2.html'
+    },
+    option2: {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step3.html'
+    }
+  };
+
+function getAndPrintHTMLChunks (options) {
+    
+https.get(options, function(response){
+    var body = ''
+    response.on('error', function (err){
+        console.log(`This is your error: ${err}`)
+    })
+
+    response.on('data', function (data){
+        body += data
+    })
+
+    response.on('end', function (){
+        console.log(body)
+    })
+  })
+  }
+  getAndPrintHTMLChunks(requestOptions.option2)
