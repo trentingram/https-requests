4de5f90d43e9991002ec141226e362763d79d082
trentingram <tingram@homeaway.com>
2019-01-14 20:38:11 +0000

    adds buffering to http request, using .on/end method

---
+++ buffer.js
@@ -0,0 +1,25 @@
+var https = require('https')
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+function getAndPrintHTMLChunks () {
+    
+https.get(requestOptions, function(response){
+    var body = 'Trent'
+    response.on('error', function (err){
+        console.log(`This is your error: ${err}`)
+    })
+
+    response.on('data', function (data){
+        body += data
+    })
+
+    response.on('end', function (){
+        console.log(body)
+    })
+  })
+  }
+  getAndPrintHTMLChunks()
