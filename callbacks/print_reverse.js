b5128251613af67d56018187e3721c7e9262ca92
trentingram <tingram@homeaway.com>
2019-01-15 00:18:36 +0000

    adds print_reverse.js

---
+++ callbacks/print_reverse.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/reverse.html'
+  };
+
+function htmlReverser(html) {
+    console.log(html.split('').reverse().join(''))
+}
+
+
+getHTML(requestOptions, htmlReverser)
